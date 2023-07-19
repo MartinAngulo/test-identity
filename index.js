b62a626e20cb593c73b2df9a3e73970e794f555d
Martin Angulo <martinangulo1194@gmail.com>
2023-07-18 20:33:35 -0500

    new proejct to test

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-certificates");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  try {
+    // Azure Key Vault configuration
+    const vaultUrl = `https://test-cert-kv.vault.azure.net/`;
+
+    // Create an instance of DefaultAzureCredential
+    const credential = new DefaultAzureCredential();
+
+    // Create an instance of SecretClient
+    const client = new SecretClient(vaultUrl, credential);
+
+    const certificateName = "sig-keys";
+
+    // Retrieve the latest version of the certificate
+    const certificate = await client.getCertificate(certificateName);
+
+    // Access the certificate's properties and values
+    console.log("Certificate:", certificate);
+
+    res.status(200).send("success");
+  } catch (err) {
+    console.log("error", err);
+    res.status(500).send("failed");
+  }
+});
+
+app.listen(3000, () => console.log("Example app is listening on port 3000."));
